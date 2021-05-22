7e4d3184931351b7ddfa1c2fa31376eb45a92023
Rohit <rohitkairamkonda123@gmail.com>
2021-05-22 23:16:01 +0530

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,10 +1,12 @@
-export class Dish {
-    id!: string;
-    name!: string;
-    image!: string;
-    category!: string;
-    featured!: boolean;
-    label!: string;
-    price!: string;
-    description!: string;
+import { Comment  } from './comments';
+ export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments: Comment[];
 }
