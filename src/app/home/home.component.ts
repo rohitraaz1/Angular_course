5d117a4ad6458b28daef025c5cf99de3f67c08e5
Rohit <rohitkairamkonda123@gmail.com>
2021-05-27 00:37:13 +0530

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,8 @@ export class HomeComponent implements OnInit {
   promotion: Promotion;
   leaders: Leader;
   disherrMess: string;
+  promerrMess: string;
+  lederrMess: string;
 
   constructor(private dishService:DishService,
     private promotionService: PromotionService, 
@@ -25,9 +27,9 @@ export class HomeComponent implements OnInit {
     this.dishService.getFeaturedDish().subscribe(dish => this.dish = dish,
       disherrMess => this.disherrMess = <any>disherrMess);
     this.promotionService.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion,
-      disherrMess => this.disherrMess = <any>disherrMess);
+      promerrMess => this.promerrMess = <any>promerrMess);
     this.leaderService.getFeaturedLeader().subscribe(leaders => this.leaders = leaders,
-      disherrMess => this.disherrMess = <any>disherrMess);
+      lederrMess => this.lederrMess = <any>lederrMess);
   }
 
 }
