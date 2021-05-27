a2725ef526660b3891e3bf48e0d2ea2c43cc71a5
Rohit <rohitkairamkonda123@gmail.com>
2021-05-27 19:32:06 +0530

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comments';
+import { animate,trigger,state,style,transition } from '@angular/animations';
   
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -18,6 +32,7 @@ export class DishdetailComponent implements OnInit {
   commentsForm: FormGroup;
   comment: Comment;
   dishCopy: Dish;
+  visibility = 'shown';
     
     dish: Dish;
     dishIds: string[];
@@ -46,9 +61,9 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds
       ,dderrorMess => this.dderrorMess = <any>dderrorMess);
 
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe( dish => {this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id)} 
-    ,dderrorMess => this.dderrorMess = <any>dderrorMess );
+      this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; }, 
+    dderrorMess => this.dderrorMess = <any>dderrorMess );
   }
 
   createForm(): void{
